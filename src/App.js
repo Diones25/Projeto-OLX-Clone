5318b4120d33795ccc8de74d62a5e6f21a167f75
Diones Pereira Alves <pereiradiones987@gmail.com>
2021-01-21 17:07:00 -0300

    Adicionando o App.css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import {BrowserRouter} from 'react-router-dom'
+import './App.css'
 import {Template} from './components/MainComponents'
 import Header from './components/partials/Header'
 import Footer from './components/partials/Footer'
