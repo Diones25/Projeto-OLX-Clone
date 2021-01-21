1bfeb56e19a4745585224f5e28e6b9a762124967
Diones Pereira Alves <pereiradiones987@gmail.com>
2021-01-21 17:36:43 -0300

    Criando o Header

--- src/components/partials/Header/index.js
+++ src/components/partials/Header/index.js
@@ -1,10 +1,19 @@
 import React from  'react'
 import {HeaderArea} from './styled'
+import {Link} from 'react-router-dom'
 
 const Header = () =>{
     return(
         <HeaderArea>
-            ...
+            <div className="container">
+                <div className="logo">
+                    <Link to="/">
+                        <span className="logo-1">O</span>
+                        <span className="logo-2">L</span>
+                        <span className="logo-3">X</span>
+                    </Link>
+                </div>
+            </div>
         </HeaderArea>
     )
 }
--- src/components/partials/Header/styled.js
+++ src/components/partials/Header/styled.js
@@ -1,6 +1,33 @@
 import styled from 'styled-components'
 
 export const HeaderArea = styled.div`
-    height: 100px;
-    background-color: #ccc;
+    background-color: #fff;
+    height: 60px;
+    border-bottom; 1px solid #ccc;
+    .container{
+        max-width: 1000px;
+        margin: auto;
+        display: flex;
+        height: 60px;
+    }
+
+    a{
+        text-decoration: none;
+    }
+
+    .logo{
+        flex: 1;
+        display: felx;
+        align-items: center;
+
+        .logo-1,
+        .logo-2,
+        .logo-3{
+            font-size: 27px;
+            font-weight: bold;
+        }
+        .logo-1{color: #ff0000}
+        .logo-2{color: #00ff00}
+        .logo-3{color: #0000ff}
+    }
 `
